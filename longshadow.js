838ec214d8bfd75a7816aafd0d396f32f1634039
Zhang Yiwen <tiotaocn@gmail.com>
2014-06-23 17:53:02 +0800

    include demo image

--- longshadow.js
+++ longshadow.js
@@ -1,51 +1,37 @@
 // helper class to shade color
-
-function shadeRGBColor(color, percent) {
-    var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
-    return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
-}
-
-function shadeColor2(color, percent) {   
-    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-}
-
-function shade(color, percent){
-    if (color.length > 7 ) return shadeRGBColor(color,percent);
-    else return shadeColor2(color,percent);
+function blendColors(c0, c1, p) {
+    var f=parseInt(c0.slice(1),16),t=parseInt(c1.slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF,R2=t>>16,G2=t>>8&0x00FF,B2=t&0x0000FF;
+    return "#"+(0x1000000+(Math.round((R2-R1)*p)+R1)*0x10000+(Math.round((G2-G1)*p)+G1)*0x100+(Math.round((B2-B1)*p)+B1)).toString(16).slice(1);
 }
 
 
-var parseCSS = function (original, array) {
+function parseCSS (original, array) {
 	var css = ', ' + array[0] + 'px ' + array[1] + 'px ' + array[2];
 	return original + css
 }
 
-var longShadow = function (target, degree, color, length, shade_ratio) {
+function longShadow (target, degree, color, background, length, shade_ratio) {
 	
-	// default
+	var attr;
+	var normal_x;
+	var normal_y;
+	var x;
+	var y;
+	var value = '0px 0px ' + color;
 
 	degree = degree % 360
 
 	if (degree === 90 || degree === 0 || degree === 180 || degree == 360 || degree == 270) {
 		degree = degree + 0.1
 	}
+	
 	shade_ratio = typeof shade_ratio !== 'undefined' ? shade_ratio : 1/15;
    	degree = typeof degree !== 'undefined' ? degree : 45;
    	length = typeof length !== 'undefined' ? length : 150;
-	
+   	background = typeof background !== 'undefined' ? background : '#FFFFFF';
 	
 	radian = degree * (Math.PI / 180);
-
    	length_ratio = Math.tan(radian);
-
-   	console.log(length_ratio)
-
-	var value = '0px 0px ' + color;
-
-	var normal_x = 1;
-	var normal_y = 1;
-
 
 	if (degree >= 0 && degree < 90) {
 		normal_x = 1
@@ -61,19 +47,19 @@ var longShadow = function (target, degree, color, length, shade_ratio) {
 		normal_y = -1
 	}
 
-	var x = 0;
-	var y = 0;
-
-
 	for (var i = 1; i < length; i++) {
-		x = i
-		y = x * length_ratio
-		x_n = x * normal_x
-		y_n = y * normal_y
-		value = parseCSS(value, [x_n, y_n, shade(color, i * shade_ratio)])
+		x = i * normal_x
+		y = i * length_ratio * normal_y
+		value = parseCSS(value, [x, y, blendColors(color, background , i * shade_ratio)])
 	}
 
-	target.css('box-shadow', value)
+	if (target.is('p, h1, h2, h3, h4, h5, h6')) {
+		attr = 'text-shadow'
+	} else {
+		attr = 'box-shadow'
+	}
+
+	target.css(attr, value)
 
 }
 
